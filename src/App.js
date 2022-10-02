d631c0bb7fd27d19559dc6384392563a5a234e26
dvoritK <101967699+dvoritK@users.noreply.github.com>
2022-10-02 13:11:00 +0300

    Added cup convertor

--- src/App.js
+++ src/App.js
@@ -1,27 +1,76 @@
 import "./App.css";
+import $ from "jquery";
+import { useState } from "react";
 
 function App() {
+  let [inputValue, setInputValue] = useState("");
+  let [cupConvertion, setCupConvertion] = useState("");
+
+  function convertToCup() {
+    switch ($("#ingredient").val()) {
+      case "sugarPowder":
+        setCupConvertion(inputValue / 120);
+        break;
+      case "whiteFlour":
+      case "cornflour":
+      case "cocoa":
+        setCupConvertion(inputValue / 140);
+        break;
+      case "wholeWheatFlour":
+        setCupConvertion(inputValue / 125);
+        break;
+      case "semolina":
+      case "demeraraSugar":
+      case "whiteSugar":
+        setCupConvertion(inputValue / 200);
+        break;
+      case "brownSugar":
+        setCupConvertion(inputValue / 240);
+        break;
+      default:
+        console.log("please type amount");
+        break;
+    }
+  }
+
+  function handleClick(event) {
+    event.preventDefault();
+    convertToCup();
+  }
+  function handleChange(event) {
+    const amount = event.target.value;
+    setInputValue(amount);
+  }
+
   return (
     <div className="App">
-      <form>
-        <input type="number" placeholder="כמות בגרמים"></input>
-        <select name="בחר/י חומר גלם">
-          <option value="קמח לבן">בחר/י חומר גלם</option>
-          <option value="">אבקת סוכר</option>
-          <option value="">סוכר לבן</option>
-          <option value="">סוכר דמררה</option>
-          <option value="">סוכר חום כוס דחוסה</option>
-          <option value="">סולת</option>
-          <option value="">פודינג</option>
-          <option value="">קוקוס</option>
-          <option value="">קורנפלור</option>
-          <option value="קמח לבן">קמח לבן</option>
-          <option value="">קמח מלא</option>
-          <option value="">קקאו</option>
-          <option value="">שיבולת שועל</option>
-          <option value="">שקדים / אגוזים טחונים</option>
+      <form onSubmit={handleClick}>
+        <input type="submit" value="חשב" />
+        <input
+          type="number"
+          placeholder="כמות בגרמים"
+          onChange={handleChange}
+        ></input>
+        <select name="בחר/י חומר גלם" id="ingredient">
+          <option value="chooseIngredient">בחר/י חומר גלם</option>
+          <option value="sugarPowder" id="sugarPowder">
+            אבקת סוכר
+          </option>
+          <option value="whiteSugar">סוכר לבן</option>
+          <option value="demeraraSugar">סוכר דמררה</option>
+          <option value="brownSugar">סוכר חום כוס דחוסה</option>
+          <option value="semolina">סולת</option>
+          <option value="pudding">פודינג</option>
+          <option value="coconut">קוקוס</option>
+          <option value="cornflour">קורנפלור</option>
+          <option value="whiteFlour">קמח לבן</option>
+          <option value="wholeWheatFlour">קמח מלא</option>
+          <option value="cocoa">קקאו</option>
+          <option value="oatmeal">שיבולת שועל</option>
+          <option value="almonds">שקדים / אגוזים טחונים</option>
         </select>
       </form>
+      <p>{cupConvertion}</p>
     </div>
   );
 }
