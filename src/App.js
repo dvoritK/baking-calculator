f9e909615b8e3f6c9a9979259875bf715ed763ca
dvoritK <101967699+dvoritK@users.noreply.github.com>
2022-10-02 09:40:40 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,22 +3,25 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <select name="בחר/י חומר גלם">
-        <option value="קמח לבן">בחר/י חומר גלם</option>
-        <option value="">אבקת סוכר</option>
-        <option value="">סוכר לבן</option>
-        <option value="">סוכר דמררה</option>
-        <option value="">סוכר חום כוס דחוסה</option>
-        <option value="">סולת</option>
-        <option value="">פודינג</option>
-        <option value="">קוקוס</option>
-        <option value="">קורנפלור</option>
-        <option value="קמח לבן">קמח לבן</option>
-        <option value="">קמח מלא</option>
-        <option value="">קקאו</option>
-        <option value="">שיבולת שועל</option>
-        <option value="">שקדים / אגוזים טחונים</option>
-      </select>
+      <form>
+        <input type="number" placeholder="כמות בגרמים"></input>
+        <select name="בחר/י חומר גלם">
+          <option value="קמח לבן">בחר/י חומר גלם</option>
+          <option value="">אבקת סוכר</option>
+          <option value="">סוכר לבן</option>
+          <option value="">סוכר דמררה</option>
+          <option value="">סוכר חום כוס דחוסה</option>
+          <option value="">סולת</option>
+          <option value="">פודינג</option>
+          <option value="">קוקוס</option>
+          <option value="">קורנפלור</option>
+          <option value="קמח לבן">קמח לבן</option>
+          <option value="">קמח מלא</option>
+          <option value="">קקאו</option>
+          <option value="">שיבולת שועל</option>
+          <option value="">שקדים / אגוזים טחונים</option>
+        </select>
+      </form>
     </div>
   );
 }
